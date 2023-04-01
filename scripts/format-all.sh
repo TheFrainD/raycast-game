9dc1e2dae0aa182d1e97b2aba8d85098ef6c5511
Dmytro Zykov <dmytri.zykov@gmail.com>
2023-04-01 15:21:35 +0300

    Fix format script

--- scripts/format-all.sh
+++ scripts/format-all.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 
-if [ -z "$1" ]; then
-  echo "Usage: $0 <root_directory> [<exclude_directory1> ...]"
-  exit 1
-fi
-
-if [ -n "${@:2}" ]; then
-  FILES=$(find "$1" -type d \( -name "${@:2}" -prune \) -o -type f \( -name "*.cpp" -o -name "*.h" \) -print)
+if [ -f .clang-format ]; then
+    FORMAT_FILE=".clang-format"
 else
-  FILES=$(find "$1" -type f \( -name "*.cpp" -o -name "*.h" \))
+    echo "Error: .clang-format file not found in current directory."
+    exit 1
 fi
 
+if [ $# -eq 0 ]; then
+    echo "Error: no directories specified."
+    echo "Usage: $0 <directory1> <directory2> ..."
+    exit 1
+fi
 
-for file in $FILES; do
-  echo "Formatting file: $file"
-  clang-format -i -style=file "$file"
+for dir in "$@"; do
+    if [ ! -d "$dir" ]; then
+        echo "Error: $dir is not a directory."
+        exit 1
+    fi
+    find "$dir" -type f \( -name "*.c" -o -name "*.cpp" -o -name "*.h" -o -name "*.hpp" \) -print0 | while IFS= read -r -d '' file; do
+        echo "Formatting $file"
+        clang-format -i -style=file -fallback-style=none -assume-filename="$FORMAT_FILE" "$file"
+    done
 done
